d820ebeb4aa60a8b73a79c20b6d36e9ee976136a
Michael Calcinai <michael@calcin.ai>
2016-07-20 19:51:21 +0200

    And the rest

--- src/CodeEvaluator.php
+++ src/CodeEvaluator.php
@@ -1,9 +1,107 @@
 <?php
 
+namespace Calcinai\Incephption;
+
+use PhpParser\Node;
+use PhpParser\ParserFactory;
+use PhpParser\PrettyPrinter\Standard;
+
 /**
  * @package    incephption
  * @author     Michael Calcinai <michael@calcin.ai>
  */
 class CodeEvaluator {
+
+    private $root_block;
+    private $printer;
+
+    public function __construct($code) {
+
+        if(substr($code, 0, 5) !== '<?php'){
+            $code = '<?php '.$code;
+        }
+
+        $parser = (new ParserFactory())->create(ParserFactory::PREFER_PHP7);
+        $nodes = $parser->parse($code);
+
+        $this->printer = new Standard();
+
+        $this->simplifyNodes($nodes);
+
+        echo $this->printer->prettyPrint($nodes);
+
+        exit;
+    }
+
+
+
+    public function simplifyNodes($nodes){
+        foreach($nodes as $node){
+            $simplified_nodes = $this->simplifyNode($node);
+
+        }
+        return $simplified_nodes;
+    }
+
+
+    public function simplifyNode($node, $static = []) {
+
+        if($node instanceof Node\Stmt\If_){
+            return $this->simplifyIf($node);
+        } else {
+            return $node;
+        }
+
+
+//
+//        foreach($nodes as $node_index => $node) {
+//
+//            if($node instanceof Node\Stmt\If_){
+//                $replacement = $this->simplifyIf($node);
+//            } else {
+//                return $nodes;
+//            }
+//
+//
+//
+//            array_splice($nodes, $node_index, 1, $replacement);
+//
+//            //Not sure if this is the best way to do this, but the splice chanes the size and mucks with the pointer.
+//            $new_index = $node_index + count($replacement);
+//            while($new_index--){
+//                next($nodes);
+//            }
+//
+//        }
+
+
+    }
+
+
+    public function simplifyIf(Node\Stmt\If_ $if){
+
+        print_r($if);
+
+        return $this->simplifyNodes($if->stmts);
+    }
+
+
+    public static function fromReflection(\ReflectionFunctionAbstract $reflection) {
+        $file = new \SplFileObject($reflection->getFileName());
+        $file->seek($reflection->getStartLine()-1);
+
+        $lines_remaining = $reflection->getEndLine() - $reflection->getStartLine();
+        $code = '';
+        while($lines_remaining--){
+            $code .= $file->getCurrentLine();
+        }
+
+        //Remove leading and trailing braces
+        $code = ltrim($code, "\n {");
+        $code = substr($code, 0, strrpos($code, '}'));
+
+        return new self($code);
+    }
+
 
 }
--- test/test-all.php
+++ test/test-all.php
@@ -5,53 +5,16 @@
  * @author     Michael Calcinai <michael@calcin.ai>
  */
 
-use Calcinai\Incephption\Node\FileNode;
-use Calcinai\Incephption\Fluent\Context\FileContext;
+
+use Calcinai\Incephption\CodeEvaluator;
 
 include __DIR__.'/../vendor/autoload.php';
 
 $condition = true;
 
-$file = new FileNode();
-(new FileContext($file))
-
-    ->namespace('Calcinai\\Test')
-
-    ->use('Vendor\\Package\\Class')
-    ->use('Vendor2\\Package\\Class')->as('NonConflict')
-
-    /**
-     * Something important about the class
-     * Another line
-     */
-    ->abstract->class('Test')->extends('Base')
-
-    ->use('SomeTrait')
-    ->use('SomeOtherTrait')
-        ->method('A')->insteadof('SomeTrait::A')
-        ->method('B')->as->private()
-        ->method('C')->as('newMethodName')
-
-    //Inline comments, too!
-    ->const('CONSTANT_ONE', 1)
-    ->const('CONSTANT_TWO', 2)
-
-    ->private->static('private_property', 'default_value')
-
-    ->public('public_property')
-
-    /**
-     * Summary for this function
-     *
-     * Params will be automatically appended below, although you can override a specific param if you need to.
-     * If you're using PHP7, you can type hint scalars too.
-     * The 'use' in the closure will disappear from generated code, with any of its variables evaluated
-     *
-     * @param string $who Override automatic doc for this variable
-     */
-    ->final->protected->function('helloWorld', function(DateTime $date, $who) use($condition){
+$function = function(DateTime $date, $who) use($condition){
         if($condition == true){
-            if($condition == $condition){
+            if($condition == $condition && time()){
                 echo $date->format('u');
             }
         } else {
@@ -65,11 +28,7 @@ $file = new FileNode();
             case false:
                 echo $who;
         }
-    })
+    };
 
-    ->public->static->function('secondFunction', function(){
-        return false;
-    })
-;
 
-echo $file;
+print_r(CodeEvaluator::fromReflection(new ReflectionFunction($function)));
